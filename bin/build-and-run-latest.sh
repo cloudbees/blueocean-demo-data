8c99bd3d0287049eddafabb6b7b80c5614bbc3e9
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2016-09-23 12:45:37 +0200

    Use blueocean/blueocean:master as latest

--- bin/build-and-run-latest.sh
+++ bin/build-and-run-latest.sh
@@ -9,4 +9,4 @@ docker build -t blueocean/blueocean-demo-data:local "$PROJECT_ROOT"
 # cleanup previous demo data volume
 docker rm -v blueocean-demo-data || true
 docker create --name blueocean-demo-data blueocean/blueocean-demo-data:local
-docker run --rm -p 8080:8080 --volumes-from blueocean-demo-data blueocean/blueocean
+docker run --rm -p 8080:8080 --volumes-from blueocean-demo-data blueocean/blueocean:master
--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,4 @@ set -eu -o pipefail
 docker rm -v blueocean-demo-data || true
 docker create --name blueocean-demo-data blueocean/blueocean-demo-data
 
-docker run --rm -p 8080:8080 --volumes-from blueocean-demo-data blueocean/blueocean
+docker run --rm -p 8080:8080 --volumes-from blueocean-demo-data blueocean/blueocean:master
